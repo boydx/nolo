b6103f8290f22ae5be4f58bb9622c6cf944eabd0
Drew Taylor <drewt@outlook.com>
2020-04-26 00:36:56 -0500

    Added base
    Copied stuff from initial RRG site, included Nolo potree implementation

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nolensville
+[Nolensville, TN Trail System](basemap/Nolensville.pdf)
+
+
+![Mill Creek](photos/b1.jpg) Mill Creek, photo by Drew Taylor
+
+![Floodplain](photos/infosign.jpg) Floodplain, photo by Drew Taylor
+
+![Retention Pond](photos/pond.jpg) Retention Pond, photo by Drew Taylor
---
+++ potree_final/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 135533585,
+    "boundingBox": {
+        "lx": 1765503.0,
+        "ly": 581378.0,
+        "lz": 566.97,
+        "ux": 1781502.99,
+        "uy": 597377.99,
+        "uz": 16566.959999999992
+    },
+    "tightBoundingBox": {
+        "lx": 1765503.0,
+        "ly": 581378.0,
+        "lz": 566.97,
+        "ux": 1779502.99,
+        "uy": 597377.99,
+        "uz": 989.32
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 138.56398010253907,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
